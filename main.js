1ddd7f9b5811d630c13a289c454ca798efd88fb6
Brian Borin <Brianborin@icloud.com>
2016-09-25 17:13:40 -0700

    changed jquery skills section

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
-console.log('working');
-
-$(document).ready(function() {
-  $('.skill-slider').hide();
-  setInterval(function(){
-    $('.skill-slider')
-    .fadeOut(500).fadeIn(2000).appendTo('.skills-content');
-  },3000)
-})
+var count = 1;
+disp();
+function disp() {
+  if (count === 1) $(".skill-slider").hide();
+  $('.sk' + count)
+    .fadeIn(2000, disp).appendTo('.skills-content');
+  count = (count % 4) + 1;
+}
